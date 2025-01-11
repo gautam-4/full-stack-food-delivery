b12d4cae6721febad7881ef297c1ea21e72a79aa
gautam-4 <gautamalt468@gmail.com>
2025-01-12 01:32:45 +0530

    fix: connection to db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,16 +3,14 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     const mongoURI = process.env.MONGO_URI;
     if (!mongoURI) {
-        throw new Error("MONGO_URI is not defined in environment variables.");
+        throw new Error("MONGO_URI is not defined");
     }
     
-    await mongoose.connect(`${mongoURI}/food-del`, { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
-    })
-    .then(() => console.log("DB Connected"))
-    .catch((err) => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("DB Connected");
+    } catch (err) {
         console.error("DB Connection Error:", err.message);
         process.exit(1);
-    });
+    }
 };
