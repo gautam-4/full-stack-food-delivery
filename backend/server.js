be9dfedc77eb692061be19e84ab6e294260fcd6d
gautam-4 <gautamalt468@gmail.com>
2025-01-11 15:27:03 +0530

    feat: create api endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express"
 import cors from 'cors'
 import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 // app config
 const app = express()
@@ -14,6 +18,13 @@ app.use(cors())
 
 // db connection
 connectDB()
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 app.get("/", (req, res) => {
   res.send("working")
